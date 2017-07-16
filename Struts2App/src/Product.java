d7a60ffca5572859546b1472ce6a8a45304d9833
ZhangJin <nbnbnb.zhang@gmail.com>
2017-07-16 10:46:52 +0800

    Updata to EE7

---
+++ Struts2App/src/Product.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+/**
+ * Created by ZhangJin on 2017/7/16.
+ */
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1000L;
+
+    private String name;
+    private String description;
+    private String price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String execute(){
+        return "success";
+    }
+}
