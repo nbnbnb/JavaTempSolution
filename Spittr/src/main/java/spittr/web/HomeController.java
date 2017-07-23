51b56eb43c5140e57992299e9ee1fcf8d037d591
ZhangJin <nbnbnb.zhang@gmail.com>
2017-07-23 16:21:03 +0800

    h2database

--- Spittr/src/main/java/spittr/web/HomeController.java
+++ Spittr/src/main/java/spittr/web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
 
   @RequestMapping(method = GET)
--- Spittr/src/test/java/spittr/web/HomeControllerTest.java
+++ Spittr/src/test/java/spittr/web/HomeControllerTest.java
@@ -1,12 +1,12 @@
 package spittr.web;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+        import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+        import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
+        import org.junit.Test;
+        import org.springframework.test.web.servlet.MockMvc;
 
-import spittr.web.HomeController;
+        import spittr.web.HomeController;
 
 public class HomeControllerTest {
 
@@ -14,8 +14,8 @@ public class HomeControllerTest {
   public void testHomePage() throws Exception {
     HomeController controller = new HomeController();
     MockMvc mockMvc = standaloneSetup(controller).build();
-    mockMvc.perform(get("/"))
-           .andExpect(view().name("home"));
+    mockMvc.perform(get("/home/"))
+            .andExpect(view().name("home"));
   }
 
 }
