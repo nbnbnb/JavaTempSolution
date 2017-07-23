4d0b1a4a475e03daeeca3a0c96c8ea803d56adf4
ZhangJin <nbnbnb.zhang@gmail.com>
2017-07-23 10:13:49 +0800

    Mvc 01 - bad

---
+++ Spittr/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,36 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc  // 启用 Spring Mvc
+@ComponentScan(basePackages = {"spittr.web"})  // 启用组件扫描
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        // 配置 JSP 视图解析器
+
+        InternalResourceViewResolver resourceViewResolver = new InternalResourceViewResolver();
+        resourceViewResolver.setPrefix("/WEB-INF/views/");
+        resourceViewResolver.setSuffix(".jsp");
+        resourceViewResolver.setExposeContextBeansAsAttributes(true);
+        return resourceViewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+
+        super.configureDefaultServletHandling(configurer);
+
+        configurer.enable(); // 配置静态资源的处理
+    }
+}
---
+++ Spittr/web/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%--
+  Created by IntelliJ IDEA.
+  User: ZhangJin
+  Date: 2017/7/8
+  Time: 18:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page session="false" %>
+
+<html>
+<head>
+    <title>Spittr</title>
+</head>
+<body>
+<h1>Welcome to Spittr</h1>
+
+</body>
+</html>
