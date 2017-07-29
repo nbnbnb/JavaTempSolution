20e7a909e1655d81200b1512b960ee31ee806277
ZhangJin <nbnbnb.zhang@gmail.com>
2017-07-30 01:32:11 +0800

    CC

--- ConsoleApp/src/main/java/Main.java
+++ ConsoleApp/src/main/java/Main.java
@@ -1,9 +1,3 @@
-import concert.ToListCollector;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 /**
  * Created by ZhangJin on 2017/7/8.
  */
@@ -16,21 +10,6 @@ public class Main {
 
     public static void Demo() {
 
-        List<Dish> bb = null;
-
-        List<Dish> dishes = bb.stream().collect(new ToListCollector<>());
-
-        bb.stream().collect(ArrayList::new, List::add, List::addAll);
     }
-
-    static long sequentialSum(long n) {
-        return Stream.iterate(1L, i -> i + 1)
-                .limit(n)
-                .parallel()
-                .reduce(0L, Long::sum);
-    }
-
 }
 
-class Dish {
-}
