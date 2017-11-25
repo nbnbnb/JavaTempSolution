41c3b45d04b597271c42f9bc6825e6c3c756790b
张进 <jinzhanga@Ctrip.com>
2017-11-25 14:52:34 +0800

    cc

--- ConsoleApp/src/main/java/demos/JavaDemo.java
+++ ConsoleApp/src/main/java/demos/JavaDemo.java
@@ -1,19 +1,11 @@
 package demos;
 
 
-import javax.annotation.Nonnull;
-import javax.annotation.meta.When;
-
 /**
  * Created by ZhangJin on 2017/11/23.
  */
 public class JavaDemo {
+    public static void test() {
 
-    public void a(@Nonnull(when = When.NEVER) String name) {
-
-    }
-
-    public void b() {
-        a(null);
     }
 }
