ea09af39b49927a6b37900e60f003520375ef958
张进 <jinzhanga@Ctrip.com>
2018-01-22 10:28:16 +0800

    Update Kotlin 1.2.20

--- ConsoleApp/src/main/java/demos/KotlinDemo.kt
+++ ConsoleApp/src/main/java/demos/KotlinDemo.kt
@@ -1,7 +1,24 @@
 package demos
 
-fun test() {
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 
+fun test() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("I'am sleepting $i")
+            delay(500)
+        }
+    }
+
+    delay(1300)
+    println("main: I'm tired of waiting!")
+    job.cancel()
+    job.join()
+
+    println("main: Now I can quit.")
 }
+
 
 
