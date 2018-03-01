cea6bf99d29b32c43d8174a121e427f7d18a58b3
张进 <jinzhanga@Ctrip.com>
2018-03-01 17:53:17 +0800

    SBConsoleApp

---
+++ SBConsoleApp/src/main/kotlin/SBConsoleApp/Main.kt
@@ -0,0 +1,24 @@
+package SBConsoleApp
+
+/**
+ * Created by ZhangJin on 2017/7/8.
+ */
+object Main {
+    private fun log(obj: Any?) {
+        println(obj)
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        log("----- start -----")
+        temp()
+        log("-----  end  -----")
+    }
+
+    private fun temp() {
+
+    }
+}
+
+
+
