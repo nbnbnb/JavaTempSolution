3b74e84bc28e22231e2a6767047824dc720de02c
张进 <jinzhanga@Ctrip.com>
2018-03-02 17:40:49 +0800

    Kotlin -> 1.2.30

--- ConsoleApp/src/main/kotlin/ConsoleApp/demos/KotlinDemo.kt
+++ ConsoleApp/src/main/kotlin/ConsoleApp/demos/KotlinDemo.kt
@@ -3,7 +3,11 @@ package ConsoleApp.demos
 object KotlinDemo {
     fun test() {
         println("Hello World!")
-        
+
+        val lazyValue: String by lazy {
+            println("computed!")
+            "Hello"
+        }
     }
 }
 
