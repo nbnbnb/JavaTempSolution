9d67c5a2a66b9f39f407ec656c8927f7e6cfc124
张进 <jinzhanga@Ctrip.com>
2018-05-29 12:25:26 +0800

    cc

--- BasicConsoleApp/src/main/kotlin/basicconsoleapp/springdemo/concert/Audience.kt
+++ BasicConsoleApp/src/main/kotlin/basicconsoleapp/springdemo/concert/Audience.kt
@@ -5,51 +5,59 @@ import org.aspectj.lang.annotation.*
 import org.springframework.stereotype.Component
 
 // 指定一个切面
+// 里面定义切入点（Pointcut）和通知（Advice）
 @Aspect
 // 声明为一个 bean
 @Component
 class Audience {
 
     // 定义一个命名的切点
-    // 方法名称可以随意取
-    // 方法的实际内容并不重要， 在这里它实际上应该是空的
-    // 其实该方法本身只是一个标识， 供 @Pointcut 注解依附
-    // 切点表达式
-    // execution 表示在方法执行的时候触发
-    // * 表示返回任意类型
-    // basicconsoleapp.Performance 表方法的类
-    // * 表示所有的方法
-    // (..) 表示使用任意参数
+    //
+    //  方法名称可以随意取
+    //  方法的实际内容并不重要， 在这里它实际上应该是空的
+    //  表达式中的匹配的方法，都会执行切入
+
+    // 切点表达式 execution(* basicconsoleapp.Performance.*(..))
+    //
+    //  execution 表示在方法执行的时候触发
+    //  * 表示返回任意类型
+    //  basicconsoleapp.Performance 表方法的类
+    //  * 表示所有的方法
+    //  (..) 表示使用任意参数
     @Pointcut("execution(* basicconsoleapp.Performance.*(..))")
     fun performance() {
     }
 
-
-    @Before("performance())")
+    // 将 @Before Advice 绑定到切入点上
+    @Before("performance()")
     fun silenceCellPhones(joinPoint: JoinPoint) {
         println("AspectJ Method Is: " + joinPoint.signature.name)
         println("Silencing cell phones")
     }
 
-    @Before("performance())")
+    // 将 @Before Advice 绑定到切入点上
+    @Before("performance()")
     fun takeSeats(joinPoint: JoinPoint) {
         println("Taking seats")
     }
 
+    // 将 @After Advice 绑定到切入点上
     @After("performance()")
     fun after(joinPoint: JoinPoint) {
         // 在原始方法执行完后执行
         println("after aspect executed")
     }
 
-    @AfterReturning(pointcut = "performance())", returning = "returnVal")
+    // 将 @AfterReturning Advice 绑定到切入点上
+    @AfterReturning(pointcut = "performance()", returning = "returnVal")
     fun applause(joinPoint: JoinPoint, returnVal: Any?) {
         // 在 @After 执行完后执行
         println("CLAP CLAP CLAP!!!")
-        println("Result is: " + returnVal)
+        println("Result is: $returnVal")
     }
 
-    @AfterThrowing("performance())")
+    // 将 @AfterThrowing Advice 绑定到切入点上
+    @AfterThrowing("performance()")
     fun demandRefund(joinPoint: JoinPoint) {
         println("Demanding a refund")
     }
--- BasicConsoleApp/src/main/kotlin/basicconsoleapp/springdemo/concert/EncoreableIntroducer.kt
+++ BasicConsoleApp/src/main/kotlin/basicconsoleapp/springdemo/concert/EncoreableIntroducer.kt
@@ -12,10 +12,15 @@ object EncoreableIntroducer {
 
     // 通过 @DeclareParents 注解
     // 将 Encoreable 接口引入到 Performance bean 中
+    //
     // value 属性指定了哪种类型的 bean 要引入该接口
-    // 标记符后面的加号表示是 Performance 的所有子类型， 而不是 Performance 本身
-    // defaultImpl 属性指定了为引入功能提供实现的类。 在这里，我们指定的是 DefaultEncoreable 提供实现。
-    // @DeclareParents 注解所标注的静态属性指明了要引入的接口。 在这里， 我们所引入的是 Encoreable 接口。
+    // 标记符后面的 + 表示是 Performance 的所有子类型， 而不是 Performance 本身
+    //
+    // defaultImpl 属性指定了为引入功能提供实现的类。
+    // 在这里，我们指定的是 DefaultEncoreable 提供实现。
+    //
+    // @DeclareParents 注解所标注的静态属性 encoreable 指明了要引入的接口。
+    // 在这里， 我们所引入的是 Encoreable 接口（DefaultEncoreable 实现了该接口）
     @DeclareParents(value = "basicconsoleapp.Performance+", defaultImpl = DefaultEncoreable::class)
     var encoreable: Encoreable? = null
 
