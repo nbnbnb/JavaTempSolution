3dc6d7b2f120aa5084417aa68cae4c68e8ad7879
张进 <jinzhanga@Ctrip.com>
2018-08-09 15:34:10 +0800

    cc

--- BasicConsoleApp/src/main/kotlin/basicconsoleapp/demos/JavaDemo.java
+++ BasicConsoleApp/src/main/kotlin/basicconsoleapp/demos/JavaDemo.java
@@ -1,13 +1,10 @@
 package basicconsoleapp.demos;
 
 
-import org.springframework.stereotype.Component;
-
 /**
  * Created by ZhangJin on 2017/11/23.
  */
 
-@Component
 public class JavaDemo {
     public static void test() {
 
