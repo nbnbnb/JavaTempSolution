d27e7a399b49b3199ad1ff46cd7c03f510f51331
张进 <jinzhanga@Ctrip.com>
2018-08-09 15:16:50 +0800

    添加 ORM JPA 依赖

--- BasicConsoleApp/src/main/kotlin/basicconsoleapp/demos/JavaDemo.java
+++ BasicConsoleApp/src/main/kotlin/basicconsoleapp/demos/JavaDemo.java
@@ -1,14 +1,17 @@
 package basicconsoleapp.demos;
 
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by ZhangJin on 2017/11/23.
  */
+
+@Component
 public class JavaDemo {
     public static void test() {
 
     }
 }
-
 
 
