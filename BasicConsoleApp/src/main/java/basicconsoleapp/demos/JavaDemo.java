ebf72798ff8c5c9d454b2a96b2e29d1e55ffb409
ZhangJin <nbnbnb.zhang@gmail.com>
2018-10-24 22:01:57 +0800

    ex

--- BasicConsoleApp/src/main/java/basicconsoleapp/demos/JavaDemo.java
+++ BasicConsoleApp/src/main/java/basicconsoleapp/demos/JavaDemo.java
@@ -1,13 +1,17 @@
 package basicconsoleapp.demos;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by ZhangJin on 2017/11/23.
  */
 
 public class JavaDemo {
 
-    public static void test() {
+    public static void test() throws Exception {
 
     }
 }
